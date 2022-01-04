1d54e13140eb6bbcf30695bd5d44f4cfa6e07abf
blevings <76407367+blevings@users.noreply.github.com>
2022-01-03 22:21:54 -0600

    Added 5 Old Testament Verse

---
+++ mixed_messages.js
@@ -0,0 +1,12 @@
+
+
+let oldTestamentVerses = ['The grass withers, the flower fades, but the word of our God will stand forever. - Isaiah 40:8',
+'Hear, O Israel: The Lord our God, the Lord is one. You shall love the Lord your God with all your heart and with all your soul and with all your might. - Deuteronomy 6:4-5',
+'And these words that I command you today shall be on your heart. You shall teach them diligently to your children, and shall talk of them when you sit in your house, and when you walk by the way, and when you lie down, and when you rise. - Deuteronomy 6:6-7',
+'Surely he has borne our griefs, and carried our sorrows; yet we esteemed him stricken, smitten by God, and afflicted. But he was pierced for our transgressions; he was crushed for our iniquities; upon him was the chastisement that brought us peace, and with his wounds we are healed. - Isaiah 53:4-5',
+'All we like sheep have gone astray; we have turned—every one—to his own way; and the Lord has laid on him the iniquity of us all. - Isaiah 53:6',
+]
+let psalmVerses = []
+let newTestamentVerses = []
+
+console.log(oldTestamentVerses[0])
